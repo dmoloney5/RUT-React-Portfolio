498f5502a5dcd033529c7fc9b807705e7f353c20
techman <dmoloney5@gmail.com>
2022-05-15 13:16:36 -0400

    finished project page

---
+++ src/components/Projects/index.js
@@ -0,0 +1,138 @@
+import React from "react";
+
+
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <section id="projects" className="project">
+        <h2 className="title">Websites</h2>
+        <div className="project" >
+          <h3>Project 1 - Run Buddy Website</h3>
+          <div className="project-img">
+            <p>
+              <a href="https://dmoloney5.github.io/run-buddy/" target="_blank">
+                <img src='images/Run_Buddy.jpg' alt="Run Buddy" style={{ display: "flex" }} width="450" height="400"/>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 2 - Password Generator</h3>
+          <div className="project-img">
+            <p>
+              <a
+                href="https://dmoloney5.github.io/RUT-Password-WEEK3/" target="_blank">
+                <img
+                  src="images/Password_Generator.jpg"
+                  alt="Generate Passwords" style={{ display: "flex" }} width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 3 - Javascript Quiz</h3>
+          <div className="project-img">
+            <p>
+              <a
+                href="https://dmoloney5.github.io/RUT-WEEK4-Quiz/" target="_blank">
+                <img
+                  src="images/Quick-Quiz.jpg"
+                  alt="Test your Knowledge of javascript" width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 4 - Taskinator</h3>
+          <div className="project-img">
+            <p>
+              <a href="https://dmoloney5.github.io/Taskniator/" target="_blank">
+                <img
+                  src="images/Taskinator.jpg"
+                  alt="Create a Kanban board" width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 5 - Chuck-N-Go - Group Project</h3>
+          <div className="project-img">
+            <p>
+              <a href="https://ang543.github.io/chuck-n-go/" target="_blank">
+                <img
+                  src="images/ChucknGo-screenshot.jpg"
+                  alt="Mapping app with Chuck Norris Jokes" width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 6 - Developer Blog</h3>
+          <div className="project-img">
+            <p>
+              <a href="https://rut-developer-blog.herokuapp.com/" target="_blank">
+                <img src="images/Developer's Blog.jpg" alt="My Blog" width="450" height="400" />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 7 - A to B - Group Project 2</h3>
+          <div className="project-img">
+            <p>
+              <a
+                href="https://a-to-b-cars-rut-proj2.herokuapp.com/homepage" target="_blank">
+                <img src="images/homepage.jpg" alt="car app" width="450" height="400"/>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 8 - Pizza-Hunt</h3>
+          <div className="project-img">
+            <p>
+              <a
+                href="https://pizza-hunt-rut.herokuapp.com/" target="_blank">
+                <img src="images/Pizza Hunt.png" alt="the pizza blog" width="450" height="400" />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 9 - Budget Tracker</h3>
+          <div className="project-img">
+            <p>
+              <a href="https://rut-week19-budget-tracker.herokuapp.com/" target="_blank">
+                <img
+                  src="images/Budget_Tracker.jpg"
+                  alt="Currently Under Construction" width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Project 10 - Under Contruction</h3>
+          <div className="project-img">
+            <p>
+              <a href="" target="_blank">
+                <img
+                  src="images/under-construction_sign.jpg"
+                  alt="Currently Under Construction" width="450" height="400"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+       
+      </section>
+    );
+  }
+}
+
+export default Projects;
